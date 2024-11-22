bd6e0d9908e402fd16fb7ec22409957eacb09e65
Loretoaa16 <loreto16@hotmail.es>
2024-11-22 19:09:57 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,21 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["The dog", "My grandma", "The mailman", "My bird"];
+  let action = [" ate", " peed", " crushed", " broke"];
+  let what = [" my homework", " my phone", " the car"];
+  let when = [
+    " before the class",
+    " when I was sleeping",
+    " while I was exercising",
+    " during my lunch",
+    " while I was praying"
+  ];
+  let po1 = Math.floor(Math.random() * who.length);
+  let po2 = Math.floor(Math.random() * action.length);
+  let po3 = Math.floor(Math.random() * what.length);
+  let po4 = Math.floor(Math.random() * when.length);
+
+  document.querySelector("#excuse").innerHTML =
+    who[po1] + action[po2] + what[po3] + when[po4];
 };
